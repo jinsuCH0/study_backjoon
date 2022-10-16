670de7a08d689d1b76868bef6bf0125504178b0f
Jin Su Cho <blacksky9795@naver.com>
2022-10-16 19:45:16 +0900

    킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ ch1_IO_and_arithmetic/no.3003.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+fun main() {
+    val sc = Scanner(System.`in`)
+    val k = sc.nextInt()
+    val q = sc.nextInt()
+    val l = sc.nextInt()
+    val v = sc.nextInt()
+    val n = sc.nextInt()
+    val p = sc.nextInt()
+    print("${1-k} ${1-q} ${2-l} ${2-v} ${2-n} ${8-p}")
+}
